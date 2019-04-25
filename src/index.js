efaa5df656fede4d63cca298e2d8e1e8cad8e382
Dave Gonzalez <dvgz@outlook.com>
2019-04-24 20:10:19 -0400

    before bringing in example code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactDataGrid from "react-data-grid";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const columns = [
+  { key: "id", name: "ID", editable: true },
+  { key: "title", name: "Title", editable: true },
+  { key: "complete", name: "Complete", editable: true }
+];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rows = [
+  { id: 0, title: "Task 1", complete: 20 },
+  { id: 1, title: "Task 2", complete: 40 },
+  { id: 2, title: "Task 3", complete: 60 }
+];
+
+class Example extends React.Component {
+  state = { rows };
+
+  onGridRowsUpdated = ({ fromRow, toRow, updated }) => {
+    this.setState(state => {
+      const rows = state.rows.slice();
+      for (let i = fromRow; i <= toRow; i++) {
+        rows[i] = { ...rows[i], ...updated };
+      }
+      return { rows };
+    });
+  };
+  render() {
+    return (
+      <ReactDataGrid
+        columns={columns}
+        rowGetter={i => this.state.rows[i]}
+        rowsCount={3}
+        onGridRowsUpdated={this.onGridRowsUpdated}
+        enableCellSelect={true}
+      />
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Example />, rootElement);
