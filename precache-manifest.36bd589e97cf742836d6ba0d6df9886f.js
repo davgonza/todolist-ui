e58018e90eff451c5fa9af702cf7293c2b1c3a64
Dave Gonzalez <dvgz@outlook.com>
2019-05-01 12:07:09 -0400

    Updates

---
+++ precache-manifest.36bd589e97cf742836d6ba0d6df9886f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "872176fe68c60b1a4ebd",
+    "url": "/todolist-ui/static/css/main.cee69853.chunk.css"
+  },
+  {
+    "revision": "872176fe68c60b1a4ebd",
+    "url": "/todolist-ui/static/js/main.9cfb12a2.chunk.js"
+  },
+  {
+    "revision": "a5eb0bddfa708fa71713",
+    "url": "/todolist-ui/static/js/runtime~main.3bb09d01.js"
+  },
+  {
+    "revision": "291caa4170a81fd0b65e",
+    "url": "/todolist-ui/static/css/2.f38ff50b.chunk.css"
+  },
+  {
+    "revision": "291caa4170a81fd0b65e",
+    "url": "/todolist-ui/static/js/2.269b1a2f.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todolist-ui/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/todolist-ui/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/todolist-ui/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/todolist-ui/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/todolist-ui/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/todolist-ui/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/todolist-ui/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/todolist-ui/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/todolist-ui/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/todolist-ui/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/todolist-ui/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/todolist-ui/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/todolist-ui/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/todolist-ui/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/todolist-ui/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/todolist-ui/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/todolist-ui/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "f33583f5929958e81daf57627dea7e53",
+    "url": "/todolist-ui/index.html"
+  }
+];
