98046f1650327c05f2174554ca85936d60ef9e9d
Dave Gonzalez <dvgz@outlook.com>
2019-04-30 22:35:17 -0400

    some more cleanup, like styling of 'done' rows; also fixed dateDisplay

--- src/App.js
+++ src/App.js
@@ -6,37 +6,51 @@ import update from 'immutability-helper';
 import Typography from '@material-ui/core/Typography'
 import { Button, ListItemText } from '@material-ui/core'
 
-
-
-
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
 
 import moment from "moment";
 import DateEditor from "./DateEditor";
-import linq from "linq";
 
-const What = ({ value }) => {
+import PropTypes from 'prop-types';
+
+const { Row } = ReactDataGrid;
+const dateDisplay = ({ value }) => {
     return value == null ? null : moment(value).format("L");
 };
 
 
 const const_columns = [
-    { key: "id", name: "ID", sortable: true, editable: true },
     { key: "title", name: "Title", sortable: true, editable: true },
     { key: "done", name: "Done", sortable: true, editable: true },
-    { key: "finish_by_date", name: "Finish by", sortable: true, editable: true, editor: DateEditor, formatter: What }
+    { key: "finish_by_date", name: "Finish by", sortable: true, editable: true, editor: DateEditor, formatter: dateDisplay }
 ];
 
+class RowRenderer extends React.Component {
+  static propTypes = {
+    idx: PropTypes.string.isRequired
+  };
 
-const const_rows = [
-    { id: 0, title: "example 1", done: 20 },
-    { id: 1, title: "example 2", done: 40 },
-    { id: 2, title: "example 3", done: 60 }
-];
+  setScrollLeft = (scrollBy) => {
+    this.row.setScrollLeft(scrollBy);
+  };
 
+  getRowStyle = () => {
+    return {
+      color: this.getRowBackground()
+    };
+  };
 
+  getRowBackground = () => {
+    return this.props.row.done == true ? 'gray' : 'blue';
+  };
+
+  render() {
+    // usually though it will just be a matter of wrapping a div, and then calling back through to the grid
+    return (<div style={this.getRowStyle()}><Row ref={ node => this.row = node } {...this.props}/></div>);
+  }
+}
 
 
 class App extends React.Component {
@@ -52,38 +66,16 @@ class App extends React.Component {
         this.addNewRow = this.addNewRow.bind(this);
     }
 
-    // default State object
     state = {
         rows: [],
         refresh: false
     };
-
-    getSize() {
-        let count = this.state.rows.length;
-
-        if (this.state.refresh) {
-            count++; // hack for update data-grid
-            this.setState({
-                refresh: false
-            });
-        }
-
-        return count;
-    }
-
-    refresh() {
-        this.setState({
-            refresh: true
-        });
-    }
-
 
     componentDidMount() {
         axios
             .get("https://cors-anywhere.herokuapp.com/https://minimal-todo-server.herokuapp.com/todos")
             .then(response => {
 
-                // create an array of rows only with relevant data
                 const newRows = response.data.map(c => {
                     return {
                         id: c.id,
@@ -93,21 +85,18 @@ class App extends React.Component {
                     };
                 });
 
-                // create a new "State" object without mutating 
-                // the original State object. 
+                var comparer = this.returnComparer('ASC', 'finish_by_date')
+
                 const newState = Object.assign({}, this.state, {
-                    rows: newRows
+                    rows: newRows.sort(comparer)
                 });
 
-                var comparer = this.returnComparer('ASC', 'id')
-                this.state.rows = newRows.sort(comparer);
-
-                // store the new state object in the component's state
                 this.setState(newState);
             })
             .catch(error => console.log(error));
-        
-        this.grid.handleSort('id', 'DESC');
+
+        // set column arrow to sort
+        this.grid.handleSort('finish_by_date', 'ASC');
     }
 
     onGridSort = (columnName, sortDirection) => {
@@ -149,6 +138,25 @@ class App extends React.Component {
         })
     }
 
+    getSize() {
+        let count = this.state.rows.length;
+
+        if (this.state.refresh) {
+            count++; // hack for update data-grid
+            this.setState({
+                refresh: false
+            });
+        }
+
+        return count;
+    }
+
+    refresh() {
+        this.setState({
+            refresh: true
+        });
+    }
+
     addNewRow = () => {
         let addNewRows = this.state.rows.slice()
 
@@ -168,21 +176,63 @@ class App extends React.Component {
 
     deleteRow = () => { 
         console.log(this.state.selectedIndexes)
+
         const selectedIndexes = this.state.selectedIndexes
+
+        if (selectedIndexes.length != 1) {
+            return;
+        }
+
+        let rowId = this.state.rows[selectedIndexes[0]].id
+
+        axios.delete(`https://cors-anywhere.herokuapp.com/https://minimal-todo-server.herokuapp.com/todos/${rowId}`)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+            
+        let filteredSelectedIndexes = []
+
         // delete one item at once
-        //this.state.rows.splice(this.state.selectedIndexes, 1)
-        // delete multiple items at once
         const filteredRows = this.state.rows.filter(function(element, index) {
             // user indexOf method to find
             if(selectedIndexes.indexOf(index) >= 0) {
+                filteredSelectedIndexes = selectedIndexes.filter(i => i !== index);
                 return false
             } else {
                 return true
             }
         })
-        this.setState({
-            rows: filteredRows
-        })
+        this.setState({ selectedIndexes: filteredSelectedIndexes });
+        this.setState({ rows: filteredRows });
+    }
+
+    markAsDone = () => { 
+        console.log(this.state.selectedIndexes)
+
+        const selectedIndexes = this.state.selectedIndexes
+
+        if (selectedIndexes.length != 1) {
+            return;
+        }
+
+        let row = this.state.rows[selectedIndexes[0]]
+        row.done = true;
+
+        let rows = this.state.rows.slice()
+
+        rows[selectedIndexes[0]] = row
+            
+        this.setState({rows});
+
+        const todo = { done: true };
+
+        let url = 'https://cors-anywhere.herokuapp.com/https://minimal-todo-server.herokuapp.com/todos/' + row.id;
+        axios.put(url, todo)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
     }
 
     returnComparer(sortDirection, columnName) {
@@ -201,30 +251,23 @@ class App extends React.Component {
         console.log(event)
     }
 
-
-
     myEditCallback = (row) => {
         console.log(row);
         
+        var finishDate = new Date(row.finish_by_date);
+            
         if (row.isNew) {
-            const t = {
-                title: row.title,
-                todo: {
-                    title: row.title
-                }
-            };
-
             try {
-            axios.post('https://cors-anywhere.herokuapp.com/https://minimal-todo-server.herokuapp.com/todos', 
-                { 
-                    title: row.title,
-                    done: row.done,
-                    finish_by_date: row.finish_by_date
-                 })
-                .then(res => {
-                    console.log(res);
-                    console.log(res.data);
-                })
+                axios.post('https://cors-anywhere.herokuapp.com/https://minimal-todo-server.herokuapp.com/todos', 
+                    { 
+                        title: row.title,
+                        done: row.done,
+                        finish_by_date: finishDate
+                    })
+                    .then(res => {
+                        console.log(res);
+                        console.log(res.data);
+                    })
             } catch (e) {
                 console.log(`:( Axios request failed: ${e}`)
             }
@@ -233,7 +276,7 @@ class App extends React.Component {
             const todo = {                
                 title: row.title,
                 done: row.done,
-                finish_by_date: row.finish_by_date
+                finish_by_date: finishDate
             };
 
             let url = 'https://cors-anywhere.herokuapp.com/https://minimal-todo-server.herokuapp.com/todos/' + row.id;
@@ -242,17 +285,15 @@ class App extends React.Component {
                     console.log(res);
                     console.log(res.data);
                 })
-            
-            todo.id = row.id;
-            const rows = this.state.rows;
-            const index = rows.indexOf(row);
-
-            rows[index] = row;
-            rows[index].canSave = false;
-
-            this.setState({ rows });
-            this.refresh();
         }
+
+        const rows = this.state.rows;
+        const index = rows.indexOf(row);
+
+        rows[index].canSave = false;
+
+        this.setState({ rows });
+        this.refresh();
     };
 
     getEditRowAction = (column, row, state) => {
@@ -261,8 +302,6 @@ class App extends React.Component {
                 {
                     icon: <span className="glyphicon glyphicon-pencil" />,
                     callback: () => {
-
-
                         this.myEditCallback(row);
                     }
                 }
@@ -278,7 +317,7 @@ class App extends React.Component {
         return (
             <div onKeyPress={event => this.keyPress(event)}>
             <Typography variant='display1'>
-            Curtains
+            To-Do List
             </Typography>
 
             <ReactDataGrid
@@ -287,6 +326,7 @@ class App extends React.Component {
             ref={grid => (this.grid = grid)}
             rowsCount={this.getSize()}
             onGridSort={this.onGridSort}
+            rowRenderer={RowRenderer}
             onGridRowsUpdated={this.onGridRowsUpdated}
             enableCellSelect={true}
             rowSelection={{
@@ -306,7 +346,8 @@ class App extends React.Component {
             />
 
             <Button onClick={this.addNewRow} variant='contained' style={{float: 'right', margin: 20}}>Add</Button>
-            <Button onClick={this.deleteRow} variant='outlined' color='secondary' style={{ margin: 20 }}>Delete Rows</Button>
+            <Button onClick={this.deleteRow} variant='outlined' color='secondary' style={{ margin: 20 }}>Delete</Button>
+            <Button onClick={this.markAsDone} variant='outlined' color='secondary' style={{ margin: 40 }}>Done</Button>
             </div>
 
         );
